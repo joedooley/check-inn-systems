ee9dea7d55f8ec066ad25dfde8025d20e31aa5b2
Joe Dooley <hello@developingdesigns.com>
2016-10-20 21:48:15 -0400

    Added WooCommerce Product Enquiry Form

--- wp-content/themes/check-inn-systems/functions.php
+++ wp-content/themes/check-inn-systems/functions.php
@@ -8,19 +8,20 @@
  */
 
 
+add_action( 'genesis_setup', 'check_inn_systems_includes_constants' );
 /**
- * Bootstrap Genesis and include theme files.
+ * Include theme files and declare child theme constants.
  */
-include_once get_template_directory() . '/lib/init.php';
-include_once __DIR__ . '/includes/output.php';
-require_once __DIR__ . '/includes/customize.php';
-include_once __DIR__ . '/includes/genesis.php';
-include_once __DIR__ . '/includes/scripts-and-styles.php';
+function check_inn_systems_includes_constants() {
 
+	if ( ! is_admin() ) {
+		include_once __DIR__ . '/includes/output.php';
+		require_once __DIR__ . '/includes/customize.php';
+		include_once __DIR__ . '/includes/genesis.php';
+		include_once __DIR__ . '/includes/scripts-and-styles.php';
 
-/**
- * Define Child Theme Constants
- */
-define( 'CHILD_THEME_NAME', 'Check Inn Systems' );
-define( 'CHILD_THEME_VERSION', '1.0.0' );
+		define( 'CHILD_THEME_NAME', 'Check Inn Systems' );
+		define( 'CHILD_THEME_VERSION', '1.0.0' );
+	}
 
+}
