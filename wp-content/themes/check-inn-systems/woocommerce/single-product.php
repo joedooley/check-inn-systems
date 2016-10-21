ce147571e6c92322ae29b10d0f58f481ce6c27c6
Joe Dooley <hello@developingdesigns.com>
2016-10-21 13:48:31 -0400

    Added icons to the primary navigation. Icons are added in style.css

--- wp-content/themes/check-inn-systems/woocommerce/single-product.php
+++ wp-content/themes/check-inn-systems/woocommerce/single-product.php
@@ -20,12 +20,6 @@
 /** Remove default Genesis loop */
 remove_action( 'genesis_loop', 'genesis_do_loop' );
 
-/** Remove WooCommerce breadcrumbs */
-//remove_action( 'woocommerce_before_main_content', 'woocommerce_breadcrumb', 20 );
-
-/** Uncomment the below line of code to add back WooCommerce breadcrumbs */
-//add_action( 'woocommerce_single_product_summary', 'woocommerce_breadcrumb', 10, 0 );
-
 remove_action( 'genesis_before_loop', 'genesis_do_breadcrumbs' );
 add_action( 'woocommerce_single_product_summary', 'genesis_do_breadcrumbs', 4 );
 
@@ -34,12 +28,8 @@ remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wr
 remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10 );
 
 /** Switch Add To Cart and Summary sections */
-remove_action( 'woocommerce_single_variation',
-	'woocommerce_single_variation_add_to_cart_button', 10 );
-remove_action( 'woocommerce_single_variation',
-	'woocommerce_single_variation_add_to_cart_button', 20 );
-//add_action( 'woocommerce_before_add_to_cart_button',
-//	'woocommerce_single_variation_add_to_cart_button', 10 );
+remove_action( 'woocommerce_single_variation', 'woocommerce_single_variation_add_to_cart_button', 10 );
+remove_action( 'woocommerce_single_variation', 'woocommerce_single_variation_add_to_cart_button', 20 );
 
 
 
@@ -48,7 +38,7 @@ remove_action( 'woocommerce_single_variation',
  */
 function acf_accordion() {
 
-	if ( have_rows( 'accordion' ) ) :
+	if ( have_rows( 'accordion' ) && is_product() ) {
 
 		echo '<div id="accordion">';
 
@@ -57,24 +47,23 @@ function acf_accordion() {
 			$heading = get_sub_field( 'header' );
 			$content = get_sub_field( 'hidden_content' ); ?>
 
-				<div class="accordion-item">
+			<div class = "accordion-item">
 
 				<?php if ( $heading ) : ?>
-					<h2 class="accordion-heading heading"><?php echo $heading; ?></h2>
+					<h2 class = "accordion-heading heading"><?php echo $heading; ?></h2>
 				<?php endif; ?>
 
-				<div class="accordion-content"><?php echo $content; ?></div>
+				<div class = "accordion-content"><?php echo $content; ?></div>
 
-				</div>
+			</div>
 
 			<?php
 
-			endwhile;
+		endwhile;
 
 		echo '</div>';
 
-	endif;
-
+	}
 }
 
 
